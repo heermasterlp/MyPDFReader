dfa426f520b33e75e52106d390a3a60e949076cd
heermasterlp <heermaster@163.com>
2015-07-31 19:47:00 +0800

    Add table view cell code!

--- MyPDFReader/PDFTableController.swift
+++ MyPDFReader/PDFTableController.swift
@@ -8,19 +8,26 @@
 
 import UIKit
 
-class PDFTableController: UITableViewController {
-    
-    var pdfFiles: [String] = [] // All pdf file name
+class PDFTableController: UITableViewController, UISearchBarDelegate {
     
     let fm = NSFileManager.defaultManager()
     
     let mainPath = NSBundle.mainBundle().resourcePath! // Documents directory
+
+    var pdfFiles: [String] = [] // All pdf file name
+    var filteredPDFFiles:[String] = [] // Filtered the pdf file name
+    
+    var searchActive: Bool = false
     
     @IBOutlet var tvPDFs: UITableView!
     
+    @IBOutlet var searchBar: UISearchBar!
     // View did load
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /* Set up the delegates */
+        searchBar.delegate = self
         
         // 1. Get the list of PDFs
         pdfFiles = listPDFFiles()
@@ -51,43 +58,98 @@ class PDFTableController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    /* Get list of PDFs */
+    func listPDFFiles() -> [String] {
+        
+        var result: [String] = []
+        if let items = fm.contentsOfDirectoryAtPath(mainPath, error: nil) as? [String] {
+            for item in items {
+                if item.hasSuffix("pdf") {
+                    // Add the file name
+                    result.append((item as NSString).substringWithRange(NSMakeRange(0, count(item) - 4)))
+                }
+            }
+        } else {
+            println("No pdf files existed!")
+        }
+        return result
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
+        if searchActive {
+            return filteredPDFFiles.count
+        }
         return pdfFiles.count
     }
-
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("PDFCell", forIndexPath: indexPath) as! UITableViewCell
-
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> PDFViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PDFCell", forIndexPath: indexPath) as! PDFViewCell
+        
         // Configure the cell...
-        cell.textLabel?.text = pdfFiles[indexPath.row]
-
-        return cell
-    }
-    
-    /* Get list of PDFs */
-    func listPDFFiles() -> [String] {
-        
-        var items = fm.contentsOfDirectoryAtPath(mainPath, error: nil) as! [String]
-        var result: [String] = []
-        
-        for item in items {
-            if item.hasSuffix("pdf") {
-                // Add the file name
-                result.append((item as NSString).substringWithRange(NSMakeRange(0, count(item) - 4)))
+        if searchActive {
+            cell.pdfLabel?.text = filteredPDFFiles[indexPath.row]
+            if let pdfPath = NSBundle.mainBundle().pathForResource(filteredPDFFiles[indexPath.row], ofType: "pdf"){
+                
+                let pdfUrl = NSURL.fileURLWithPath(pdfPath)
+                cell.uiwebView.loadRequest(NSURLRequest(URL: pdfUrl!))
+            }
+        } else {
+            cell.pdfLabel?.text = pdfFiles[indexPath.row]
+            if let pdfPath = NSBundle.mainBundle().pathForResource(pdfFiles[indexPath.row], ofType: "pdf"){
+                
+                let pdfUrl = NSURL.fileURLWithPath(pdfPath)
+                cell.uiwebView.loadRequest(NSURLRequest(URL: pdfUrl!))
             }
         }
-        return result
+        
+        return cell
+    }
+
+    
+    /* Search Bar Actions */
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filteredPDFFiles = pdfFiles.filter({ (text) -> Bool in
+            let tmp: NSString = text
+            let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+            return range.location != NSNotFound
+        })
+        if filteredPDFFiles.count == 0 {
+            searchActive = false
+        } else {
+            searchActive = true
+        }
+        self.tvPDFs.reloadData()
+    }
+    
+    // called when text starts editing
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true
+    }
+    // called when text ends editing
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false
+    }
+    // called when keyboard search button pressed
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false
+    }
+    // called when cancel button pressed
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false
     }
 
 
---
+++ MyPDFReader/PDFViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PDFViewCell.swift
+//  MyPDFReader
+//
+//  Created by PeterLiu on 7/31/15.
+//  Copyright (c) 2015 PeterLiu. All rights reserved.
+//
+
+import UIKit
+
+class PDFViewCell: UITableViewCell {
+    
+   
+    @IBOutlet var uiwebView: UIWebView!
+
+    @IBOutlet var pdfLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
